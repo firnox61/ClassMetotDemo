35d51814c48f8d6e0ad1b4f549520d18ce02fb27
firnox61@gmail.com <firnox61@gmail.com>
2021-01-17 16:21:19 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Siralama(Musteri[] musteri)
+        {
+            foreach (Musteri musteriler in musteri)
+            {
+                Console.WriteLine(musteriler.Ad+"\n"+musteriler.Soyad+"\n"+musteriler.Id);
+            }
+        }
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine($"{musteri.Ad} {musteri.Soyad} {musteri.Id}Eklendi");
+        }
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine($"{musteri.Ad} {musteri.Soyad} {musteri.Id}silindi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2 = new Musteri();
+            Musteri musteri3 = new Musteri();
+            musteri1.Ad = "ismail";
+            musteri1.Soyad = "Eroglu";
+            musteri1.Id = 561834556;
+
+            musteri2.Ad = "Enes";
+            musteri2.Soyad = "Seroglu";
+            musteri2.Id = 564524556;
+
+            musteri3.Ad = "Sencer";
+            musteri3.Soyad = "Güroglu";
+            musteri3.Id = 5234856;
+
+            MusteriManager Yonetim = new MusteriManager();
+            Musteri[] musteriler = new Musteri[]{musteri1,musteri2,musteri3};
+            Yonetim.Siralama(musteriler);
+            Yonetim.Ekle(musteri1);
+            Yonetim.Silme(musteri3);
+
+
+
+        }
+    }
+}
